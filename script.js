842c83088388382091673e1efbac88351f6b9676
Diego Rodriguez <diego25598@gmail.com>
2023-11-08 20:57:53 +0100

    def update

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    // Show hidden sections when user clicks on wellcome button
-
     const button = document.querySelector(".button-53");
     const sections = document.querySelectorAll("section.hidden");
     const navLinks = document.querySelectorAll("li.hidden");
@@ -13,26 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
         navLinks.forEach(li => { li.classList.remove("hidden"); });
 
         button.style.display = "none";
-    }
-    
-    );
 
-    // Animate sections when user mouses over them
-
-    const section = document.querySelector('section.hidden');
-
-    let sectionWidth = 100;
-
-    function animar() {
-
-        sectionWidth += 1;
-
-        section.style.sectionWidth = sectionWidth + 'px';
-        
-        if (sectionWidth < 150) { requestAnimationFrame(animar); }
-
-    }
-
-    section.addEventListener('click', () => { animar(); });
+    });
 
 });
