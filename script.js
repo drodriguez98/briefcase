d7e31cfeeb9dd2cd9383cc397b318fa2143c509d
Diego Rodriguez <diego25598@gmail.com>
2023-11-08 16:26:06 +0100

    Add nav to html and css files and update event listener in script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
+    
     const button = document.querySelector(".button-53");
     const sections = document.querySelectorAll("section.hidden");
+    const navLinks = document.querySelectorAll("li.hidden");
 
     button.addEventListener("click", function() {
-        sections.forEach(section => {
-            section.classList.remove("hidden");
-        });
+
+        sections.forEach(section => { section.classList.remove("hidden"); });
+
+        navLinks.forEach(li => { li.classList.remove("hidden"); });
 
         button.style.display = "none";
-    });
+    }
+    
+    );
 });
