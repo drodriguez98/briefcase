e3295b156ea9350f4560f42df4a30a2ba6f42343
Diego Rodriguez <diego25598@gmail.com>
2023-11-09 01:04:40 +0100

    Update styles

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const button = document.querySelector(".button-53");
     const sections = document.querySelectorAll("section.hidden");
-    const navLinks = document.querySelectorAll("li.hidden");
+    const nav = document.querySelector("nav.hidden");
 
     button.addEventListener("click", function() {
 
         sections.forEach(section => { section.classList.remove("hidden"); });
-
-        navLinks.forEach(li => { li.classList.remove("hidden"); });
-
+        
         button.style.display = "none";
+
+        nav.classList.remove("hidden");
 
     });
 
