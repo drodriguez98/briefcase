30a23a20cdd053a739f5062181a618920bdc82cc
Diego Rodriguez <diego25598@gmail.com>
2023-11-13 11:06:28 +0100

    Fixed center element errors

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-// Script para resaltar el enlace activo en la barra de navegación
-
 document.addEventListener("DOMContentLoaded", function () {
 
     const navLinks = document.querySelectorAll("#navigation a");
+    const firstSectionId = "section1"; 
+
+    // Muestra la primera sección al cargar la página
+
+    const firstSection = document.getElementById(firstSectionId);
+
+    if (firstSection) { firstSection.style.display = "block"; }
 
     navLinks.forEach(link => {
 
         link.addEventListener("click", function (event) {
 
-        event.preventDefault();
+            event.preventDefault();
 
-        // Oculta todas las secciones
+            // Oculta todas las secciones
 
-        document.querySelectorAll("#main section").forEach(section => { section.style.display = "none"; });
+            document.querySelectorAll("#main section").forEach(section => { section.style.display = "none"; });
 
-        // Muestra la sección correspondiente
+            // Muestra la sección correspondiente
 
-        const targetSectionId = link.getAttribute("href").substring(1);
-        const targetSection = document.getElementById(targetSectionId);
+            const targetSectionId = link.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetSectionId);
 
-        if (targetSection) { targetSection.style.display = "block"; }
+            if (targetSection) { targetSection.style.display = "block"; }
 
-        // Resalta el enlace activo
+            // Resalta el enlace activo
 
-        navLinks.forEach(navLink => { navLink.classList.remove("active"); });
-        link.classList.add("active");
+            navLinks.forEach(navLink => { navLink.classList.remove("active"); });
+
+            link.classList.add("active");
 
         });
+
     });
+
 });
