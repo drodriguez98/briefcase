3a1aeff2969a254efa6c60617e127c15e1fd85d8
Diego Rodriguez <diego25598@gmail.com>
2023-11-17 22:07:57 +0100

    add basic contact form and link

--- script.js
+++ script.js
@@ -48,21 +48,90 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Función para mostrar el proyecto actual
     function showCurrentProject() {
-        // ... (código para mostrar el proyecto actual)
+        const projectsSubsection = document.querySelectorAll(".section2-subsection");
+
+        // Oculta todos los proyectos
+
+        projectsSubsection.forEach(itemProjectsSubsection => { itemProjectsSubsection.style.display = "none"; });
+
+        // Muestra el proyecto actual
+
+        projectsSubsection[currentItemProjectsSubsection].style.display = "block";
+
+        // Mostrar solo el botón correspondiente al inicio o final de la sección de proyectos
+
+        if (currentItemProjectsSubsection === 0) {
+
+            prevProjectBtn.style.display = "none";
+            nextProjectBtn.style.display = "block";
+
+        } else if (currentItemProjectsSubsection === projectsSubsection.length - 1) {
+
+            prevProjectBtn.style.display = "block";
+            nextProjectBtn.style.display = "none";
+            
+        } else {
+
+            prevProjectBtn.style.display = "block";
+            nextProjectBtn.style.display = "block";
+
+        }
     }
 
     // Botón para avanzar al siguiente proyecto
     if (nextProjectBtn) {
+
         nextProjectBtn.addEventListener("click", function () {
-            // ... (código para avanzar al siguiente proyecto)
+
+            const projectsSubsection = document.querySelectorAll(".section2-subsection");
+
+            // Oculta el proyecto actual
+
+            projectsSubsection[currentItemProjectsSubsection].style.display = "none";
+
+            // Avanza al siguiente proyecto
+
+            currentItemProjectsSubsection = (currentItemProjectsSubsection + 1) % projectsSubsection.length;
+
+            // Muestra el siguiente proyecto
+
+            showCurrentProject();
+
+            // Desplaza suavemente hacia arriba al cambiar de sección
+
+            const targetElement = document.getElementById("container");
+            targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
         });
+
     }
 
     // Botón para retroceder al proyecto anterior
     if (prevProjectBtn) {
+
         prevProjectBtn.addEventListener("click", function () {
-            // ... (código para retroceder al proyecto anterior)
+
+            const projectsSubsection = document.querySelectorAll(".section2-subsection");
+
+            // Oculta el proyecto actual
+
+            projectsSubsection[currentItemProjectsSubsection].style.display = "none";
+
+            // Retrocede al proyecto anterior
+
+            currentItemProjectsSubsection = (currentItemProjectsSubsection - 1 + projectsSubsection.length) % projectsSubsection.length;
+
+            // Muestra el proyecto anterior
+
+            showCurrentProject();
+
+            // Desplaza suavemente hacia arriba al cambiar de sección
+
+            const targetElement = document.getElementById("container");
+            targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
         });
+
     }
 
     // Mostrar formulario al hacer clic en el enlace de Gmail
