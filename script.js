67e12f8ba364bf088c93c2e4b82987c4fc7da185
Diego Rodriguez <diego25598@gmail.com>
2023-11-17 17:21:16 +0100

    format code

--- script.js
+++ script.js
@@ -1,73 +1,82 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     const navLinks = document.querySelectorAll("#navigation a");
     const firstSectionId = "section1";
-    // const projectsContainer = document.getElementById("projects-container");
     const nextProjectBtn = document.getElementById("next-project-btn");
+
     let currentProjectIndex = 0;
 
     // Muestra la primera sección al cargar la página
+
     const firstSection = document.getElementById(firstSectionId);
-    if (firstSection) {
-        firstSection.style.display = "block";
-    }
+
+    if (firstSection) { firstSection.style.display = "block"; }
 
     navLinks.forEach(link => {
+
         link.addEventListener("click", function (event) {
+
             event.preventDefault();
 
             // Oculta todas las secciones
-            document.querySelectorAll("#main section").forEach(section => {
-                section.style.display = "none";
-            });
+
+            document.querySelectorAll("#main section").forEach(section => { section.style.display = "none"; });
 
             // Muestra la sección correspondiente
+
             const targetSectionId = link.getAttribute("href").substring(1);
             const targetSection = document.getElementById(targetSectionId);
 
-            if (targetSection) {
-                targetSection.style.display = "block";
-            }
+            if (targetSection) { targetSection.style.display = "block"; }
 
             // Resalta el enlace activo
-            navLinks.forEach(navLink => {
-                navLink.classList.remove("active");
-            });
+            navLinks.forEach(navLink => { navLink.classList.remove("active"); });
 
             link.classList.add("active");
 
             // Muestra la primera subsección de proyectos al cambiar a la sección de proyectos
-            if (targetSectionId === "section2") {
-                showCurrentProject();
-            }
+
+            if (targetSectionId === "section2") { showCurrentProject(); }
+
         });
+
     });
 
     // Función para mostrar el proyecto actual
+
     function showCurrentProject() {
+
         const projects = document.querySelectorAll(".project-subsection");
 
         // Oculta todos los proyectos
-        projects.forEach(project => {
-            project.style.display = "none";
-        });
+
+        projects.forEach(project => { project.style.display = "none"; });
 
         // Muestra el proyecto actual
+
         projects[currentProjectIndex].style.display = "block";
     }
 
     // Botón para avanzar al siguiente proyecto
+
     if (nextProjectBtn) {
-        nextProjectBtn.addEventListener("click", function () {
-            const projects = document.querySelectorAll(".project-subsection");
+
+        nextProjectBtn.addEventListener("click", function () { const projects = document.querySelectorAll(".project-subsection");
 
             // Oculta el proyecto actual
+
             projects[currentProjectIndex].style.display = "none";
 
             // Avanza al siguiente proyecto
+
             currentProjectIndex = (currentProjectIndex + 1) % projects.length;
 
             // Muestra el siguiente proyecto
+
             showCurrentProject();
+
         });
+
     }
+    
 });
